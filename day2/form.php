ac5a68d10c869bcc68c632304bdaa9f7cbc009b5
Vkorat1012 <84782747+Vkorat1012@users.noreply.github.com>
2021-05-27 17:28:09 +0530

    Day 2
    Basic php codes

---
+++ day2/form.php
@@ -0,0 +1,18 @@
+
+<html>
+
+<body>
+<form action="oddeven1.php" method="get">
+<table>
+<tr>
+<td>Enter number</td>
+<td><input type="textbox" name="txt1"></td>
+</tr>
+<tr><td><input type="submit"name="txt2"></td></tr>
+</table>
+</form>
+</body>
+</html>
+
+
+
---
+++ day2/oddeven1.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$a=$_GET["txt1"];
+if($a%2==0){
+    echo "number is even";
+}
+else{
+    echo"number is odd";
+}
+
+?>
